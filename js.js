512172dbf400b2420cdae73abbacc9b24dd6d016
= <=>
2024-05-20 14:28:50 +0300

    gonyi5

---
+++ js.js
@@ -0,0 +1,7 @@
+ const registerButton = document.querySelector("a.register");
+
+  registerButton.addEventListener("click", () => {
+    const isAdmin = alert("Если вы админ то вы не заполняете форму а выбираете что вы админ");
+
+   
+  });
